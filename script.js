13721934db2ac2893afe72106eb8e847c699b037
parthkamal <kamalparth40@gmail.com>
2021-12-31 06:03:31 +0530

    full size completed

---
+++ script.js
@@ -0,0 +1,16 @@
+let questionString = document.getElementsByClassName("question");
+for (let i = 0; i < questionString.length; i++) {
+  const question = questionString[i];
+//   console.log(question)
+console.log(question.childNodes[1].childNodes[1].childNodes[3])
+  question.addEventListener("click", () => {
+      console.log(this)
+    let panel=question.childNodes[1].childNodes[1].childNodes[3]
+    console.log(panel)
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
